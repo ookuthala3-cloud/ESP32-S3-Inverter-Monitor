bd23cb5205adfe8d250ab3ce8dcd42b87d3ed2b7
ookuthala3-cloud <ookuthala3@gmail.com>
2026-08-12 19:10:03 +0630

    Adjust delays and modify line drawing parameters

--- src/main.cpp
+++ src/main.cpp
@@ -6,28 +6,35 @@ TFT_eSPI tft = TFT_eSPI();
 void setup()
 {
     Serial.begin(115200);
-    delay(1000);
+    delay(1500);
 
+    Serial.println();
+    Serial.println("===== TFT_eSPI TEST =====");
     Serial.println("BOOT OK");
 
     pinMode(TFT_BL, OUTPUT);
     digitalWrite(TFT_BL, TFT_BACKLIGHT_ON);
 
     Serial.println("BACKLIGHT OK");
+    delay(500);
 
     Serial.println("Starting TFT_eSPI...");
+
     tft.init();
 
     Serial.println("TFT_eSPI INIT OK");
 
     tft.setRotation(1);
 
+    Serial.println("RED");
     tft.fillScreen(TFT_RED);
     delay(1500);
 
+    Serial.println("GREEN");
     tft.fillScreen(TFT_GREEN);
     delay(1500);
 
+    Serial.println("BLUE");
     tft.fillScreen(TFT_BLUE);
     delay(1500);
 
@@ -41,7 +48,7 @@ void setup()
     tft.setTextColor(TFT_WHITE, TFT_BLACK);
     tft.drawString("MONITOR", 120, 100, 4);
 
-    tft.drawFastHLine(30, 125, 180, TFT_DARKGREY);
+    tft.drawFastHLine(25, 125, 190, TFT_DARKGREY);
 
     tft.setTextColor(TFT_GREEN, TFT_BLACK);
     tft.drawString("TFT_eSPI OK", 120, 165, 4);
